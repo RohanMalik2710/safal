bdf743b528a9f15ac04d0d7f8afec4ad140f757d
Abhishek-Sood <119648563+Abhishek-Sood@users.noreply.github.com>
2024-02-02 21:20:50 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,22 @@
-// Home.jsx
 import React from 'react';
-import HomeSlider from './HomeSlider';
-import '../styles/Home.css'
+import '../styles/Home.css'; 
 
 const Home = () => {
   return (
-    <div className='home-container'>
-      <HomeSlider />
-      <h1>Welcome to Safal!</h1>
-      <p>Empowering skilled and unskilled laborers with quality employment opportunities.</p>
-      <h2>Our Mission</h2>
-      <p>
-        At Safal, we are committed to bridging the gap between employers and skilled/unskilled laborers.
-        Our mission is to provide a user-friendly platform that facilitates quick and quality employment opportunities,
-        creating a caring and humane society.
-      </p>
+    <div className="home-container">
+      <div className="image-container">
+        <video autoPlay loop muted>
+          <source src="src/assets/home.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="content-container">
+        <div className="title">
+          <h1 id="title">Be The Fastest In Getting Mazdoor at <span id="doorStep">DoorStep</span></h1>
+        </div>
+        <p id="description">To provide an easy platform for builders to hire the laborers</p>
+        <div id="button">Find Mazdoor</div>
+      </div>
     </div>
   );
 };
